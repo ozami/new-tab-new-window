3d1fc07c96c1beb19f52da15fc26b3e9d202c57f
ozawa.masayuki <ozawa.masayuki@gmail.com>
2015-02-23 03:48:59 +0000

    * Speed up window opening, at the cost of dropping feature to open a new tab in same window.

--- bg.js
+++ bg.js
@@ -1,28 +1,20 @@
 chrome.tabs.onCreated.addListener(function(tab){
-    if (tab.url == "chrome://newtab/"){
+    if (tab.index == 0){
         return;
     }
-    if (tab.url == "about:blank"){
-        return;
-    }
-    chrome.tabs.query({windowId: tab.windowId}, function(tabs){
-        if (tabs.length <= 1){
-            return;
-        }
-        chrome.windows.get(tab.windowId, function(curWindow){
-            getOption("newWindowsPosition", function(newWindowsPosition){
-                var createData = {
-                    tabId: tab.id,
-                    incognito: tab.incognito
-                };
-                if (newWindowsPosition){
-                    createData.top = Math.max(0, curWindow.top);
-                    createData.left = Math.max(0, curWindow.left);
-                }
-                chrome.windows.create(createData, function(newWindow){
-                    chrome.windows.update(newWindow.id, {
-                        state: curWindow.state
-                    });
+    chrome.windows.get(tab.windowId, function(curWindow){
+        getOption("newWindowsPosition", function(newWindowsPosition){
+            var createData = {
+                tabId: tab.id,
+                incognito: tab.incognito
+            };
+            if (newWindowsPosition){
+                createData.top = Math.max(0, curWindow.top);
+                createData.left = Math.max(0, curWindow.left);
+            }
+            chrome.windows.create(createData, function(newWindow){
+                chrome.windows.update(newWindow.id, {
+                    state: curWindow.state
                 });
             });
         });
